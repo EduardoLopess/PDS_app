9f6539efee6aa9633ba640a3f00e28d40c0c3cfc
Leonardo Lopes <leonardo.loliveira@rede.ulbra.br>
2025-04-13 03:49:44 -0300

    add sabores drink

--- appPedidos/src/screens/listagem-pedidos/Index.jsx
+++ appPedidos/src/screens/listagem-pedidos/Index.jsx
@@ -1,10 +1,19 @@
 import { View } from "react-native"
 import { Toast } from "../../utils/notificacao/toast/Index"
 import ListaPedidoStyle from "./style"
+import { Pedido } from "../../components/pedidos/Index"
 
 export const PedidosScreen = () => {
     return (
         <View style = {ListaPedidoStyle.container}>
+            <Pedido/>
+            <Pedido/>
+            <Pedido/>
+
+            
+
+         
+            
 
         </View>
     )
